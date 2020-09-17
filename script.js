1a87425262ca79e5d634e98ec4605068684d59c6
Jonathan Stillman <jonathanstillman86@yahoo.com>
2020-09-16 22:38:27 -0400

    UPDATED AJAX FUNCTION
    Updated AJAX Function for 2nd API

--- script.js
+++ script.js
@@ -29,15 +29,12 @@ $("#photoButton").on("click", function (event) {
 
 // API KEY (YELP): MJ5-bJhkCujBxJVttLJ0vxzLgs5ge3wRewkDm32IiqjBo69dYZkqZ-oz679UL0x0bTYNRiJHmfBO1JXrB8W7b4djrkYhgMFUgpRmcdhcy4NMbz7SSqbEhpeDo8NiX3Yx
 
-var yelpURL = "http://api.yelp.com/v3/businesses/search?location=nashville";
+var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location=boston";
 
 $.ajax({
     url: yelpURL,
     method: "GET",
-    dataType: "json", 
-    beforeSend: function (xhr) {
-        xhr.setRequestHeader('Authorization', 'Bearer MJ5-bJhkCujBxJVttLJ0vxzLgs5ge3wRewkDm32IiqjBo69dYZkqZ-oz679UL0x0bTYNRiJHmfBO1JXrB8W7b4djrkYhgMFUgpRmcdhcy4NMbz7SSqbEhpeDo8NiX3Yx');
-    },
+    headers: {"Authorization": "Bearer MJ5-bJhkCujBxJVttLJ0vxzLgs5ge3wRewkDm32IiqjBo69dYZkqZ-oz679UL0x0bTYNRiJHmfBO1JXrB8W7b4djrkYhgMFUgpRmcdhcy4NMbz7SSqbEhpeDo8NiX3Yx"},
     success: function (data) {
         console.log(data)
     }
